94ba0203f964f2d50b6d3b8436822b786ff9f969
Guilherme Borges Arimatea <guilhermea@spi.local>
2020-02-11 16:55:55 -0300

    epilogue error fixed

--- index.js
+++ index.js
@@ -2,16 +2,16 @@
 const { install, writeFile } = require('./bin/main');
 const text = require('./bin/writeServer');
 
-const argv = require('yargs')
-    .scriptName("restify-creat-app")
+const yargs = require('yargs')
+    .scriptName("restify-create-app")
     .usage("$0 <cmd> [args]")
     .command("new", 'Install all Restify dependencies and create a HTTP server', () => {
         install();
         writeFile(text);
     })
-    .epilogue()
+    .epilogue('To more information check the GitHub Repository https://github.com/NerdGui1/restify-create-app')
     .help('help')
     .alias('h', 'help')
     .argv;
 
-module.exports = argv;
+module.exports = yargs;
